a02b2061811d52e27229804ca3cbf120d479dc4c
Mikkel Gravgaard <mikkel@klokke.dk>
2019-07-24 00:59:53 +0200

    default to test runtime

--- test/create_runtime.sh
+++ test/create_runtime.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-runtime_name=${RUNTIME_NAME:-lumo-runtime}
+runtime_name=${RUNTIME_NAME:-lumo-runtime-test}
 zipfile=$(mktemp -u).zip
 
 zip -j $zipfile bootstrap ${LUMO_BIN_PATH} runtime.cljs
