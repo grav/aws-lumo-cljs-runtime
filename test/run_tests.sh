efab9abdf061ba2ad3393a37cdb020e3b2d2036e
Mikkel Gravgaard <mikkel@klokke.dk>
2019-07-24 01:18:14 +0200

    run dependency test with 'inline' code

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,6 +4,8 @@ set -euxo pipefail
 
 runtime=$RUNTIME
 role=$ROLE
+
+# aws sdk
 fname=lumo-s3
 zipfile=$(mktemp -u).zip
 response_file=$(mktemp)
@@ -18,3 +20,22 @@ aws lambda create-function --function-name $fname --zip-file fileb://$zipfile \
   --runtime provided --role $role --handler aws-sdk-example.core/list-buckets
 aws lambda update-function-configuration --function-name $fname --layers "$runtime"
 aws lambda invoke --function-name $fname --payload '{}' $response_file | jq -e '.FunctionError | not' || ( cat $response_file && exit 1 )
+
+# jar-dependency in code
+fname=lumo-dep-code
+zipfile=$(mktemp -u).zip
+response_file=$(mktemp)
+
+( cd examples/lib-example &&
+zip -qr $zipfile lib_example lib
+)
+
+aws lambda delete-function --function-name $fname 2> /dev/null || true
+aws lambda create-function --function-name $fname --zip-file fileb://$zipfile \
+  --runtime provided --role $role --handler lib-example.core/handler
+aws lambda update-function-configuration --function-name $fname --layers "$runtime"
+aws lambda invoke --function-name $fname --payload '{}' $response_file | jq -e '.FunctionError | not' || ( cat $response_file && exit 1 )
+
+
+# jar-dependency in separate layer
+# TODO
