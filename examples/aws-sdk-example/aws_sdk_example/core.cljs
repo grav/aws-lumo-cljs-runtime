b4d191a919fba0a03802914bd1978a02bdb7c0f6
Mikkel Gravgaard <mikkel@klokke.dk>
2019-08-05 17:28:01 +0200

    wip

--- examples/aws-sdk-example/aws_sdk_example/core.cljs
+++ examples/aws-sdk-example/aws_sdk_example/core.cljs
@@ -3,7 +3,13 @@
 ;; important to do this outside the handler,
 ;; else we'll probably get a time-out ->
 ;; https://stackoverflow.com/a/54823567/202538
-(def aws (js/require "aws-sdk"))
+
+#_(let [req' js/require]
+  (set! goog.global.require
+    (fn [s]
+      (req' (str "/opt/nodejs/node_modules/" s)))))
+
+(def aws (js/require "/opt/nodejs/node_modules/aws-sdk"))
 
 (defn list-buckets [_]
   (js/Promise. (fn [resolve reject]
