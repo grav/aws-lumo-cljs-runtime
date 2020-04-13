7ce3e4dccbff60a1b7d62412ef80707b28b3c321
Mikkel Gravgaard <mikkel@klokke.dk>
2020-04-13 21:39:24 +0200

    wip

--- examples/http-example/make_deps_layer.sh
+++ examples/http-example/make_deps_layer.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+##########################################################
+# This script creates a lambda layer with dependencies   #
+# from a deps.edn by invoking clj to compute a classpath #
+##########################################################
+
 set -euo pipefail
 
 mkdir -p lib
 
 deps=$(clj -Spath)
+
+# extract important parts of classpath (ignore src and org/clojure/*)
 jars=$(echo ${deps//:/ } \
   | tr " " "\n" \
   | grep -v src \
