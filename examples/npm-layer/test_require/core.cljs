09bca4848ba7eb8090514c8612f20578bbe09e82
Mikkel Gravgaard <mikkel@klokke.dk>
2020-04-14 23:49:39 +0200

    wip

--- examples/npm-layer/test_require/core.cljs
+++ examples/npm-layer/test_require/core.cljs
@@ -1,5 +1,7 @@
 (ns test-require.core)
 
-(defn run-tests []
-    (assert (= 42 (.tell_me (js/require "meaning-of-life"))))
-    true)
+(def is-odd? (js/require "is-odd"))
+
+(defn meaning-of-life []
+  (.tell_me (js/require "meaning-of-life")))
+  
